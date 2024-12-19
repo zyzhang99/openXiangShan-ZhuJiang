1b25548ffb28c0b0ab58445824bdb5802b4ea9c8
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2024-12-19 17:23:35 +0800

    feat(ut): add unit top scripts of eject buffer and single channel tap.

--- src/test/lua/xmake.lua
+++ src/test/lua/xmake.lua
@@ -11,3 +11,27 @@ target("TestTLUL2ChiBridge")
 
     set_values("cfg.top", "TLUL2ChiBridge")
     set_values("cfg.lua_main", "./unitest/TLUL2ChiBridge.lua")
+
+target("TestEb")
+    add_rules("verilua")
+    add_toolchains("@vcs")
+
+    add_files(
+        build_dir .. "/Eb/*.sv",
+        "./common/*.lua"
+    )
+
+    set_values("cfg.top", "EjectBufferSNP")
+    set_values("cfg.lua_main", "./unitest/EjectBuffer.lua")
+
+target("TestSct")
+    add_rules("verilua")
+    add_toolchains("@vcs")
+
+    add_files(
+        build_dir .. "/Sct/*.sv",
+        "./common/*.lua"
+    )
+
+    set_values("cfg.top", "SingleChannelTapLocalSNP")
+    set_values("cfg.lua_main", "./unitest/SingleChannelTap.lua")
